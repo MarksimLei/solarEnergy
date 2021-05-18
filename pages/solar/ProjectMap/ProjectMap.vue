ec55db14d4c44f1b852a711240a25a666dddb667
xzl <2369941859@qq.com>
2021-05-18 15:59:16 +0800

    地图ing

--- pages/solar/ProjectMap/ProjectMap.vue
+++ pages/solar/ProjectMap/ProjectMap.vue
@@ -2,11 +2,12 @@
 	<view>
 		<view class="uni-common-mt">
 			<view>
-				<map :latitude="latitude" :longitude="longitude" scale='3' 
-				@tap='tap' @markertap='tab1'
+				<!-- @tap='tap' @markertap='tab1'
 				@labeltap='tap2' @anchorpointtap='tab3'
-				@poitap='tap4'
-				:markers="covers" :style='{"height":calcHeight+"px"}'>
+				@poitap='tap4' :markers="covers"-->
+				<map :latitude="latitude" :longitude="longitude" scale='3' 
+				:show-location="true"
+					:style='{"height":calcHeight+"px"}' id="map1" ref="map1">
 				</map>
 			</view>
 		</view>
@@ -16,11 +17,12 @@
 	export default {
 		data() {
 			return {
-				calcHeight:'300',
+				calcHeight: '300',
 				title: 'map',
 				latitude: 39.909,
 				longitude: 116.39742,
 				covers: [{
+					id: 1,
 					latitude: 39.9085,
 					longitude: 116.39747,
 					// #ifdef APP-PLUS
@@ -45,26 +47,90 @@
 			let _this = this
 			uni.getSystemInfo({
 				success: function(res) {
-					_this.calcHeight = res.windowHeight 
+					_this.calcHeight = res.windowHeight
 					// #ifdef H5
 					_this.calcHeight = res.windowHeight - 30
 					// #endif
 				}
 			})
+			let mapContext = uni.createMapContext('map1', this)
+			// mapContext.moveToLocation({
+			// 	latitude: 26.07421,
+			// 	longitude: 119.29647,
+			// 	success: e => {
+			// 		console.log('moveToLocation', e)
+			// 	}
+			// })
+			
+			// mapContext.translateMarker({
+			// 	markerId: 1,
+			// 	destination: {
+			// 		latitude: 26.07421,
+			// 		longitude: 119.29647,
+			// 	},
+			// 	autoRotate: true,
+			// 	duration: 100,
+			// 	success: e => {
+			// 		console.log('translateMarker', e)
+			// 	}
+			// })
+
+			// uni.openLocation({
+			// 	latitude: 26.07421,
+			// 	longitude: 119.29647,
+			// 	success:e=>{
+			// 		console.log('openLocation',e)
+			// 	}
+			// })
+			// uni.getLocation({
+			// 	type: 'gcj02',
+			// 	success(e) {
+			// 		const latitude = e.latitude;
+			// 		const longitude = e.longitude;
+
+			// 		// _this.l('getLocation',e)
+			// 		// mapContext.moveToLocation({
+			// 		// 	latitude : e.latitude,
+			// 		// 	longitude : e.longitude,
+			// 		// 	success:e=>{
+			// 		// 		_this.l('moveToLocation',{mapContext,e})
+			// 		// 		mapContext.translateMarker({
+			// 		// 			markerId:12345,
+			// 		// 			destination:{
+			// 		// 				latitude : e.latitude,
+			// 		// 				longitude : e.longitude,
+			// 		// 			},
+			// 		// 			success:e=>{
+			// 		// 				_this.l('translateMarker',{e})
+			// 		// 			}
+			// 		// 		})
+			// 		// 	}
+			// 		// })
+			// 	}
+			// })
 		},
 		methods: {
-			tap(e){
-				this.l('tap',e)
+			tap(e) {
+				this.l('tap', e)
+				let _this = this
+
+				uni.chooseLocation({
+					success: function(e) {
+						_this.l('chooseLocation', e)
+					}
+				})
 			},
-			tap1(e){
-				this.l('tap1',e)
-			},tap2(e){
-				this.l('tap2',e)
+			tap1(e) {
+				this.l('tap1', e)
 			},
-			tap3(e){
-				this.l('tap3',e)
-			},tap4(e){
-				this.l('tap4',e)
+			tap2(e) {
+				this.l('tap2', e)
+			},
+			tap3(e) {
+				this.l('tap3', e)
+			},
+			tap4(e) {
+				this.l('tap4', e)
 			}
 		}
 	}
--- plugins/fn.js
+++ plugins/fn.js
@@ -2,9 +2,11 @@ export default {
 	install:Vue =>{
 		Vue.prototype.l = (...args)=>{
 			console.log('log hook:',...args)
+			console.log(new Error().stack.split(/\n/g)[2])
 		}
 		Vue.prototype.i = (...args)=>{
 			console.info('info hook:',...args)
+			console.log(new Error().stack.split(/\n/g)[2])
 		}
 		Vue.prototype.e = (...args)=>{
 			console.error('error hook:',...args)
